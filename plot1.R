beb563c560dddef4cf107b8c2a34f71e1b4a35dd
tudor-bodea <tudor_bodea@hotmail.com>
2015-02-04 01:43:16 -0500

    Load Files

--- plot1.R
+++ plot1.R
@@ -1,1 +1,21 @@
-test
+rm(list=ls(all=TRUE))
+setwd("C:/Work/Personal/Personal/Coursera/4-ExploratoryDataAnalysis/wk1_Project1/ExData_Plotting1")
+
+##### LOAD DATA SET
+data <- read.table(file="household_power_consumption.txt", header=TRUE, sep=";",
+	na.strings="?", colClasses=c("character","character",	rep("numeric", times=7)), 
+	check.names=FALSE)
+# SELECT SEQUENCE OF DTES OF INTEREST
+data <- data[data[,1] %in% c("1/2/2007","2/2/2007"),]
+# FORMAT Date AND Time COLUMNS
+data <- transform(data, DateTime = strptime(paste(data[,1], data[,2]), format="%d/%m/%Y %H:%M:%S"))
+data[,1] <- as.Date(data[,1], format="%d/%m/%Y")
+# SOME QUALITY CHECKS
+str(data)
+head(data)
+tail(data)
+
+##### CREATE PLOT
+png(filename="plot1.png")
+hist(data[,3], col=2, xlab="Global Active Power (kilowatts)", main="Global Active Power")
+dev.off()
